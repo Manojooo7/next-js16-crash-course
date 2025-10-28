e8cab5c2e3b38d2f6b77199961cb688a1c47697b
Mars-int <mars.international.999@gmail.com>
2025-10-28 10:47:46 +0530

    Added database connection

---
+++ database/index.ts
@@ -0,0 +1,8 @@
+// DB MODEL
+export {default as EventModel} from './event.model';
+export {default as BookingModel} from './booking.model';
+
+
+// MODEL TYPES
+export type {IEvent} from './event.model';
+export type {IBooking} from './booking.model';
